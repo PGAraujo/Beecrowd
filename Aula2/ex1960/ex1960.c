4e1351bd1fd80f6b521844e3c57de271a0fa93e0
Pedro Gabriel <pedro.gabriel.araujo08@gmail.com>
2026-09-07 22:40:25 -0300

    ex 1960

---
+++ Aula2/ex1960/ex1960.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    scanf("%d", &N);
+
+
+    int centenas = N / 100;
+    int dezenas  = (N % 100) / 10;
+    int unidades = N % 10;
+
+    switch (centenas) {
+        case 1: printf("C"); break;
+        case 2: printf("CC"); break;
+        case 3: printf("CCC"); break;
+        case 4: printf("CD"); break;
+        case 5: printf("D"); break;
+        case 6: printf("DC"); break;
+        case 7: printf("DCC"); break;
+        case 8: printf("DCCC"); break;
+        case 9: printf("CM"); break;
+    }
+
+    switch (dezenas) {
+        case 1: printf("X"); break;
+        case 2: printf("XX"); break;
+        case 3: printf("XXX"); break;
+        case 4: printf("XL"); break;
+        case 5: printf("L"); break;
+        case 6: printf("LX"); break;
+        case 7: printf("LXX"); break;
+        case 8: printf("LXXX"); break;
+        case 9: printf("XC"); break;
+    }
+
+    switch (unidades) {
+        case 1: printf("I"); break;
+        case 2: printf("II"); break;
+        case 3: printf("III"); break;
+        case 4: printf("IV"); break;
+        case 5: printf("V"); break;
+        case 6: printf("VI"); break;
+        case 7: printf("VII"); break;
+        case 8: printf("VIII"); break;
+        case 9: printf("IX"); break;
+    }
+
+    printf("\n");
+
+    return 0;
+}
